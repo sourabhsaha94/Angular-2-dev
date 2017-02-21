bee18e68fff32c415b8fa28b40c424d92b45699a
Sourabh Saha <sssaha2@ncsu.edu>
2017-02-21 13:06:01 -0500

    tour of heroes done

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -3,17 +3,24 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
 import { AppComponent }  from './app.component';
 import {HeroesComponent} from './heroes.component';
 import {HeroDetailComponent} from './hero-detail.component';
 import {HeroService} from './hero.service';
 import {RouterModule} from '@angular/router';
 import {DashboardComponent} from './dashboard.component';
+import {HeroSearchComponent} from './hero-search.component';
+
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './in-memory-data.service';
+
 import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, AppRoutingModule],
-  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule, HttpModule,InMemoryWebApiModule.forRoot(InMemoryDataService)],
+  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent,HeroSearchComponent ],
   bootstrap:    [ AppComponent ],
   providers:    [ HeroService]
 })
--- angular-tour-of-heroes/src/app/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes.component.ts
@@ -40,4 +40,15 @@ export class HeroesComponent implements OnInit{
   ngOnInit():void{
     this.getHeroes(); //This code runs as soon as the component is executed
   }
+
+  add(name:string):void{
+    name = name.trim();
+    if(!name){return;}
+
+    this.heroService.create(name).then(hero => {this.heroes.push(hero);this.selectedHero=null});
+  }
+
+  delete(hero:Hero):void{
+    this.heroService.delete(hero.id).then(()=>{this.heroes = this.heroes.filter(h=>h!==hero);if(this.selectedHero==hero){this.selectedHero=null;}});
+  }
 }
